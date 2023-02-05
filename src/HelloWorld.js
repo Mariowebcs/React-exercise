1e31d791a61fec9ee0e191a73528e02fa09e2d00
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-02-05 18:43:34 +0100

    add : added exercise-07

--- src/HelloWorld.js
+++ src/HelloWorld.js
@@ -3,7 +3,10 @@ import React from "react";
 export class HelloWorld extends React.Component {
     render() {
         return (
-            <h1>Hello,World!!</h1>
+            <div>
+                <h1>Hello,World!!</h1>
+                <p>What a beautiful day!</p>
+            </div>
         )
     }
 }
