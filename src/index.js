8a611fa8783f41fa6a3897199a340142518a2f37
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-02-05 17:45:42 +0100

    added first exercise

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+const Hello = <h1>Hello World!</h1>;
+
+root.render(Hello);
