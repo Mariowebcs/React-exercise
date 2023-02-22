c4cf1516e33cf10a33dc3208551185d54abbfa01
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-02-22 02:33:06 +0100

    fix: fixed exercise-28 changeInputHandler about exercise-27 and added reset button

---
+++ src/Login.js
@@ -0,0 +1,67 @@
+import React from "react";
+
+export default class Login extends React.Component {
+  state = {
+    email: "",
+    password: "",
+    rememberMe: false,
+  };
+
+  changeInputHandler = (event) => {
+    const name = event.target.name;
+    this.setState({
+        [name] : event.target.value
+    })
+  }
+
+
+  clickResetHandler = () => {
+    this.setState({
+        email : '',
+        password : '',
+        rememberMe : false
+    })
+  }
+
+  clickHandler = () => {
+    const loginData = {
+      email: this.state.email,
+      password: this.state.password,
+      rememberMe: this.state.rememberMe,
+    };
+    this.props.onLogin(loginData);
+  }
+
+  render() {
+    return (
+      <div>
+        <label htmlFor="emailInput">Email : </label>
+        <input
+          type="email"
+          id="email-Input"
+          name="email"
+          onChange={this.changeInputHandler}
+          value={this.state.email}
+        />
+        <label htmlFor="passInput">Password : </label>
+        <input
+          type="password"
+          id="passInput"
+          name="password"
+          onChange={this.changeInputHandler}
+          value={this.state.password}
+        />
+        <label htmlFor="rememberMe">Ricordami</label>
+        <input
+          type="checkbox"
+          id="rememberMe"
+          name="rememberMe"
+          onChange={this.changeInputHandler}
+          checked={this.state.rememberMe}
+        />
+        <button onClick={this.clickHandler}>Login now</button>
+        <button onClick={this.clickResetHandler}>Reset</button>
+      </div>
+    );
+  }
+}
