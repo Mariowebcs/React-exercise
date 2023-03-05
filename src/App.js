7806082a8e19a797928b9411183e71b7a557eb95
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-03-05 19:37:00 +0100

    added exercise-54

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
-import React from 'react';
+import React from "react";
+import CarDetails from "./CarDetails";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+      <CarDetails
+        initialdata={{ model: "Ford focus", year: 2021, color: "green" }}
+      />
+    </div>
+  );
 };
-
 
 export default App;
