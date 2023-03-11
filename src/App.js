cfd9d66085dbed815847eb2855a61603701f2ea4
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-03-11 23:24:18 +0100

    code indented

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Routes, Route,Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Welcome from "./Welcome";
 import ClickCounter from "./ClickCounter";
 import GithubUser from "./GithubUser";
@@ -7,15 +7,22 @@ import GithubUser from "./GithubUser";
 const App = () => {
   return (
     <div>
-    <Link to="/">Home</Link>
-    <Link to="/ClickCounter">ClickCounter</Link>
-    <Link to="/users/Mariowebcs">Github User Details</Link>
+      <Link to="/">Home</Link>
+      <Link to="/ClickCounter">ClickCounter</Link>
+      <Link to="/users/Mariowebcs">Github User Details</Link>
       <Routes>
         <Route path="/" element={<Welcome name="Giada" />} />
         <Route path="/ClickCounter" element={<ClickCounter />} />
-        <Route path="/users/:username" element={<GithubUser/>} />
-        <Route path="*" element={<div><p>Not found</p></div>}/>
-      </Routes> 
+        <Route path="/users/:username" element={<GithubUser />} />
+        <Route
+          path="*"
+          element={
+            <div>
+              <p>Not found</p>
+            </div>
+          }
+        />
+      </Routes>
     </div>
   );
 };
