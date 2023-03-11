20d4cca7919686a046e546d3d18f36f07f09414e
Mario D'Andrea <mariodandrea2595@gmail.com>
2023-03-11 23:09:55 +0100

    added exercise-61

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Routes, Route,Link } from "react-router-dom";
+import Welcome from "./Welcome";
+import ClickCounter from "./ClickCounter";
+import GithubUser from "./GithubUser";
 
 const App = () => {
-    return (
-        <div>
-            
-        </div>
-    )
+  return (
+    <div>
+    <Link to="/">Home</Link>
+    <Link to="/ClickCounter">ClickCounter</Link>
+    <Link to="/users/Mariowebcs">Github User Details</Link>
+      <Routes>
+        <Route path="/" element={<Welcome name="Giada" />} />
+        <Route path="/ClickCounter" element={<ClickCounter />} />
+        <Route path="/users/:username" element={<GithubUser/>} />
+      </Routes> 
+    </div>
+  );
 };
-
 
 export default App;
